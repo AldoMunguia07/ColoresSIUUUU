d4315392e09ac9db90151cea24a831c63bd99ae7
Osman Gomez <gomezosman05@gmail.com>
2021-03-26 21:50:21 -0600

    Listo

---
+++ ColoresSIUUUU/Azul.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ColoresSIUUUU
+{
+    /// <summary>
+    /// Interaction logic for Azul.xaml
+    /// </summary>
+    public partial class Azul : Window
+    {
+        public Azul()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
+    }
+}
